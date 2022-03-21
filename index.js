d239d0f8911bacaf2cc35eeb2438dcc216c022f0
charles johnson <dagoclmt@gmail.com>
2022-03-21 13:38:09 -0700

    Scope and Closure Project Done

--- index.js
+++ index.js
@@ -27,9 +27,9 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   
   Study the code for counter1 and counter2, then answer the questions below.
   
-  1. What is the difference between counter1 and counter2? Counter is funtion scoped & counter 2 is global scoped
+  1. What is the difference between counter1 and counter2? Counter is funtion scoped & counter 2 is global scoped.
   
-  2. Which of the two uses a closure? How can you tell? Counter 1 is using closure by accessing the variable within it own enviorment(aka function)
+  2. Which of the two uses a closure? How can you tell? Counter 1 you can tell by line 41 'return function counter()' 
   
   3. In what scenario would the counter1 code be preferable? 
      counter2 be better?  
